2b5ea1e461c0d15e4d2367b7356656f04216cd6b
Nerova <OO3803@gmail.com>
2018-01-05 14:36:10 +0800

    File Control

--- FileStream/FileStream/FileControl.h
+++ FileStream/FileStream/FileControl.h
@@ -8,12 +8,27 @@
 
 using namespace std;
 
+//temporary class definition
+class Host {};
+class Player {};
+
 class FileControl {
 	public:
 		FileControl (string);
 		void read ();
-		void write (string, string);
-		void split (string, string, vector<string> &);
+		void write (string, string); 
+		// ([variable name], [data])
+		void split (string, string, vector<string> &); 
+		// ([string source], [delimiter], [output vector])
+
+		void pkgrcv (string, Host &);
+		void pkgrcv (string, Player &);
+		string pkgsnd ();
+
+		void decode (vector<vector<string>> &, Host &);
+		void decode (vector<vector<string>> &, Player &);
+
+		void encode ();
 
 	private:
 		ifstream FILE_INPUT;
