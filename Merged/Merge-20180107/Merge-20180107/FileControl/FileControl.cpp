8bde9272f8a13e33515d6885891099eece653d46
Nerova <OO3803@gmail.com>
2018-01-12 17:21:21 +0800

    update

--- Merged/Merge-20180107/Merge-20180107/FileControl/FileControl.cpp
+++ Merged/Merge-20180107/Merge-20180107/FileControl/FileControl.cpp
@@ -85,7 +85,21 @@ void FileControl::pkgrcv (string pkg, Host & host) { //(on Host side) to receive
 				break;
 			case 17: //decideCard: string
 				if (host.statement/10==2) {
-					if (pos == position && list[i].size()==3){
+					if (pos == position && list[i].size()==3) {
+						if (list[i][2]!="00" && host.turn<4) {
+							//host.Card[pos].erase ( remove ( host.Card[pos].begin(), host.Card[pos].end(), list[i][2] ), host.Card[host.turn].end() );
+							for (int j=0; j<host.Card[pos].size(); j++) {
+								if (list[i][2]==host.Card[pos][j]) {
+									host.trick_log[host.ns_trick + host.ew_trick][host.turn] = list[i][2];
+									host.Card[pos].erase(host.Card[pos].begin()+j);
+									host.statement = 20 + (pos+1)%4;
+									host.turn++;
+									break;
+								}
+							}
+						}
+					}
+					else if ((pos+2)%4 == position && position == host.declarer_position && list[i].size()==3) {
 						if (list[i][2]!="00" && host.turn<4) {
 							//host.Card[pos].erase ( remove ( host.Card[pos].begin(), host.Card[pos].end(), list[i][2] ), host.Card[host.turn].end() );
 							for (int j=0; j<host.Card[pos].size(); j++) {
